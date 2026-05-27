f2db915dd116a41a02e423fcefba76402c5cf56d
Saitejasri <saitejasrisaga99@gmail.com>
2026-05-27 22:15:11 +0530

    Init commit

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+#integer
+a = 10
+print(type(a))
+
+#float
+b = 10.5
+print(type(b))
+
+#string
+name = "Python"
+print(type(name))
+
+#boolean
+x = True
+print(type(x))
+
+#input and output
+name = input("Enter your name: ")
+print("Welcome", name)

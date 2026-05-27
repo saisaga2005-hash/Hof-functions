a3c02f1ae70f18d78ac81489a694816f22394348
Saitejasri <saitejasrisaga99@gmail.com>
2026-05-27 23:56:24 +0530

    Init commit

---
+++ basics/loops.py
@@ -0,0 +1,21 @@
+#loops
+for i in range(1, 6):
+    print(i)
+
+
+#while loop
+i = 1
+while i <= 7:
+    print(i)
+    i += 1
+#2
+i=1
+while i<=5:
+    print("CVCORP")
+    i+=1
+#3
+n=20
+i=5
+while i<=5:
+    print(i*2) #10
+    i+=1
